dd160a5993eaeb691a33fc9eb64e16a0da9e29c3
Antonov Vyacheslav <islava787@gmail.com>
2022-05-19 15:07:07 +0300

    adding componets

---
+++ src/components/Drawer.js
@@ -0,0 +1,42 @@
+
+function Drawer(){
+    return(
+        <div className="overlay d-none">
+        <div className="drawer">
+          <h2 className="mb-40">Кошик  <img className="drawer__remove-btn" src="/img/btn-remove.svg" alt="remove"/></h2>
+              <div className="drawer__items">
+                  <div className="cardItem">
+                    <img className="mr-20" width={70} height={70} src="/products/la-roshe.jpg" alt="product"/>
+                      <div className="mr-20">
+                        <p className="mb-5">La roche posay Effaclar Gel Exfoliante Puro 400ml</p>
+                        <b>666 грн</b>
+                      </div>
+                    <img className="drawer__remove-btn" src="/img/btn-remove.svg" alt="remove"/>
+                  </div>
+                  <div className="cardItem">
+                    <img className="mr-20" width={70} height={70} src="/products/la-roshe.jpg" alt="product"/>
+                      <div className="mr-20">
+                        <p className="mb-5">La roche posay Effaclar Gel Exfoliante Puro 400ml</p>
+                        <b>666 грн</b>
+                      </div>
+                    <img className="drawer__remove-btn" src="/img/btn-remove.svg" alt="remove"/>
+                  </div>
+              </div>
+                <div className="drawer__total">
+                  <ul>
+                    <li className="d-flex mb-30">
+                      <span>Вартість</span>
+                      <div></div>
+                      <b>12223грн.</b>
+                    </li>
+                  </ul>
+                  <button className="greenButton">
+                    Оформлення заказу 
+                    <img src="/img/arrow.svg" alt="arrow" /></button>
+              </div>
+        </div>
+      </div>
+    );
+}
+
+export default Drawer
---
+++ src/components/Slider.js
@@ -0,0 +1,8 @@
+function Slider(){
+    return(
+        <section className="slider">
+        <div className="slider-bg" ></div>
+        </section>
+    );
+}
+export default Slider
